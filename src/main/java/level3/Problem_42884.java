ae456317112aad68bc740959611b52011ce4cc88
Hyunsoul37 <soulhyun37@gmail.com>
2024-11-26 01:06:44 +0900

    [ADD] 42884 문제 풀이

--- src/main/java/level3/Problem_42884.java
+++ src/main/java/level3/Problem_42884.java
@@ -1,36 +1,38 @@
 package level3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Problem_42884 {
     public int solution(int[][] routes)
     {
         List<Integer> cameras = new ArrayList<>();
 
-        // 이동 거리가 짧은순
-        //Arrays.sort(routes, (route1, route2) -> (route2[0] - route2[1]) - (route1[0] - route1[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 
-        // 일찍 나간 순
-        Arrays.sort(routes, (route1, route2) -> route1[1] - route2[1]);
-        cameras.add(routes[0][1]);
-
-        for (int i = 0; i < routes.length; i++)
+        for (int[] route : routes)
         {
-            boolean found = true;
+            pq.offer(route);
+        }
 
-            for (int camera = 0; camera < cameras.size(); camera++)
+        while (!pq.isEmpty())
+        {
+            int[] route = pq.poll();
+            System.out.println(route[0] + " " + route[1]);
+            boolean isChecked = false;
+
+            for (int i = 0; i < cameras.size(); i++)
             {
-                if (routes[i][0] <= cameras.get(camera) && routes[i][1] >= cameras.get(camera))
-                    found = true;
-                else
-                    found = false;
+                if (route[0] <= cameras.get(i) && route[1] >= cameras.get(i))
+                {
+                    isChecked = true;
+                    break;
+                }
             }
 
-            if (!found)
-                cameras.add(routes[i][1]);
+            if (!isChecked)
+            {
+                cameras.add(route[1]);
+            }
         }
 
         return cameras.size();
@@ -39,7 +41,7 @@ public class Problem_42884 {
     public static void main(String[] args) {
         Problem_42884 p = new Problem_42884();
 
-        int[][] routes = new int[][]{{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}};
+        int[][] routes = new int[][]{{-20, -15}, {-14, -5}, {-18, -13}, {-5, -3}};
 
         System.out.println(p.solution(routes));
     }
